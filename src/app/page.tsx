667b81c71d8a851ada6bdf9dbd08db638d6ad5a1
4kyl_biz (Lee Li) <124536105+4kyl-biz@users.noreply.github.com>
2024-12-23 14:47:36 +1100

    Add shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Home Page</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Home Page
+      <Button variant={"link"}>Click Me!</Button>
+    </h1>
+  );
 }
