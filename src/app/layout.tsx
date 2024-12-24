c8f57e54dfc72ecb9652b47288c00be89b513d24
4kyl_biz (Lee Li) <124536105+4kyl-biz@users.noreply.github.com>
2024-12-24 12:49:52 +1100

    Add not-found for locale layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,100 +1,10 @@
-import "@/styles/global.css";
-import { NextIntlClientProvider } from "next-intl";
-import {
-  getMessages,
-  getTranslations,
-  setRequestLocale,
-} from "next-intl/server";
-import { routing } from "@/i18n/routing";
-import GlobalHeader from "@/components/GlobalHeader";
-import GlobalFooter from "@/components/GlobalFooter";
-import { Separator } from "@/components/ui/separator";
-
-export async function generateMetadata({
-  params,
-}: {
-  params: Promise<{ locale: string }>;
-}) {
-  const { locale } = await params;
-  const t = await getTranslations({ locale, namespace: "Metadata" });
-
-  return {
-    title: {
-      template: `%s | ${t("brand_name")}`,
-      default: t("brand_name"), // a default is required when creating a template
-    },
-    icons: [
-      {
-        rel: "android-chrome-192x192",
-        url: "/android-chrome-192x192.png",
-      },
-      {
-        rel: "android-chrome-512x512",
-        url: "/android-chrome-512x512.png",
-      },
-      {
-        rel: "apple-touch-icon",
-        url: "/apple-touch-icon.png",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        url: "/favicon-32x32.png",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "16x16",
-        url: "/favicon-16x16.png",
-      },
-      {
-        rel: "icon",
-        url: "/favicon.ico",
-      },
-    ],
-  };
-}
-
-export async function generateStaticParams() {
-  return routing.locales.map((locale) => ({ locale }));
-}
-
-export default async function RootLayout({
+// Since we have a root `not-found.tsx` page, a layout file
+// is required, even if it's just passing children through.
+// See https://next-intl.dev/docs/environments/error-files for more information.
+export default function RootLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: Promise<{ locale: "en" | "zh" }>;
 }) {
-  const { locale } = await params;
-
-  // Enable static rendering
-  setRequestLocale(locale);
-
-  // Providing all messages to the client
-  // side is the easiest way to get started
-  const messages = await getMessages();
-
-  return (
-    <html lang={locale}>
-      <body>
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <div className="px-16 flex flex-col min-h-screen justify-between">
-            <section>
-              <GlobalHeader locale={locale} />
-              <Separator />
-            </section>
-
-            <section>{children}</section>
-
-            <section>
-              <Separator />
-              <GlobalFooter />
-            </section>
-          </div>
-        </NextIntlClientProvider>
-      </body>
-    </html>
-  );
+  return children;
 }
